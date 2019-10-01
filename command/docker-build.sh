e9884a69c1da5a462e9faab6b839b2e43673ce33
memset0 <memset0@outlook.com>
2019-10-01 09:38:21 +0800

    fix.

--- command/docker-build.sh
+++ command/docker-build.sh
@@ -1,4 +1,3 @@
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=oi-archive --format="{{.ID}}"))
 git pull
 docker image build . -t oi-archive
 docker container run -p 8080:8080 -it oi-archive
